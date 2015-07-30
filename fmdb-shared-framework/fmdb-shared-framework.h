c8807e5c19076ee0f2d703bfc0f5f0e49b95600f
Andrew Breckenridge <asbreckenridge@me.com>
2015-07-30 14:26:48 -0700

    add shared framework

---
+++ fmdb-shared-framework/fmdb-shared-framework.h
@@ -0,0 +1,19 @@
+//
+//  fmdb-shared-framework.h
+//  fmdb-shared-framework
+//
+//  Created by Andrew Breckenridge on 7/30/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for fmdb-shared-framework.
+FOUNDATION_EXPORT double fmdb_shared_frameworkVersionNumber;
+
+//! Project version string for fmdb-shared-framework.
+FOUNDATION_EXPORT const unsigned char fmdb_shared_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fmdb_shared_framework/PublicHeader.h>
+
+
